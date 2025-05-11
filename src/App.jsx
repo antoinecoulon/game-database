09492d02640e4910a5bfcde9e06ed121cef91525
Antoine COULON <a.antoinecoulon@gmail.com>
2025-05-11 22:16:26 +0200

    Add Search bar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import logo from "/logo.png"
+import Search from "./components/Search";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [searchQuery, setSearchQuery] = useState("");
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className="w-full p-2 flex flex-col items-center">
+      <div className="flex items-center">
+        <h1 className="text-gradient text-4xl font-bold">Game Database</h1>
+        <img src={logo} alt="Logo" className="w-25 h-25" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="w-full max-w-4xl">
+        <Search 
+          query={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </main>
   )
 }
 
